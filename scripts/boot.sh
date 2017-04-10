11c19b036f5a29fcfa9a89f92ed6670d9e6d96e7
jenzus <jens@codefront.nl>
2017-04-10 19:51:36 +0200

    Created a boot script for the @reboot cron

---
+++ scripts/boot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sleep 10 
+cd /home/pi/Big-Brother-0w/wlan0-driver/ 
+./install_driver.sh 
+cd /home/pi/Big-Brother-0w/scripts
+sleep 2 
+nexutil -m2 
+sleep 2 
+sudo ./capture_probereq.sh
