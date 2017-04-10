386bb75de7015a4ecf410098bc0c1fab6dd69223
jenzus <jens@codefront.nl>
2017-04-10 20:01:39 +0200

    Added filters to the capture script, a random neighbourhood printer is spamming requests.

--- scripts/capture_probereq.sh
+++ scripts/capture_probereq.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo tshark -i wlan0 -T fields -E separator=, -E quote=d  -e wlan.sa_resolved -e wlan.sa -e wlan_mgt.ssid -e frame.time_epoch -e radiotap.dbm_antsignal  -Y "wlan.fc.type_subtype == 4 and !(wlan_mgt.ssid == \"\")" >> /home/pi/probereqs.log
+sudo tshark -i wlan0 -T fields -E separator=, -e wlan.sa_resolved -e wlan.sa -e wlan_mgt.ssid -e frame.time_epoch -e radiotap.dbm_antsignal  -Y "wlan.fc.type_subtype == 4 and !(wlan_mgt.ssid == \"\")" -f "(ether host not 00:1e:8f:a3:83:79) and (ether host not ac:cf:23:12:db:75)" >> /home/pi/probereqs.log
