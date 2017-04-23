9b8845cdaf5a2d8fbc1108cf33a535259a8f86e5
Jens van de Graaf <jens-jo@live.nl>
2017-04-23 21:30:42 +0200

    Update post-data.py

--- scripts/post-data.py
+++ scripts/post-data.py
@@ -7,7 +7,7 @@ if os.path.exists('/home/pi/probereqs.log'):
     with open('/home/pi/probereqs.log') as f:
         probedata = f.read()
 
-    url = 'https://bb.w4p.us/brother.php'
+    url = '_API_URL'
     values = { 'device': socket.gethostname(), 'data': probedata } 
     data = urllib.urlencode(values)
 
